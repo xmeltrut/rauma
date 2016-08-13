2d59610519d2bfeb83813b2d292b65851ca4b4db
Chris Worfolk <chris@worfolk.co.uk>
2016-08-13 22:38:59 +0100

    Throw error if config file does not exist

--- src/Framework/Config/ConfigFactory.php
+++ src/Framework/Config/ConfigFactory.php
@@ -9,6 +9,10 @@ class ConfigFactory
 {
     public static function load($configFile)
     {
+        if (!file_exists($configFile)) {
+            throw new \Exception(sprintf("Unable to load config file '%s'", $configFile));
+        }
+
         try {
             $yaml = new Parser();
             $value = $yaml->parse(file_get_contents($configFile));
